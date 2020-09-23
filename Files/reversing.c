1e701c3ef18e0f1f2a3b58d7605ca1f5ca729470
HarisaranG <harithiru2018@gmail.com>
2020-09-23 14:24:03 +0530

    Updated Files

--- Files/reversing.c
+++ Files/reversing.c
@@ -15,5 +15,11 @@ int main() {
     strrev(filecontent);
     printf("%s", filecontent);
     fclose(file);
+    FILE* file1;
+    file1 = fopen("file1.txt", "w");
+    fputs(filecontent, file1);
+    fclose(file1);
+    remove("file.txt");
+    rename("file1.txt", "file.txt");
     return 0;
 }
